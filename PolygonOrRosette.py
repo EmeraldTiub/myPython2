528c517ea5e8bc5ba5cdd35dc9d407bd6bea30ee
Little Tiubs Founder <86202719+LittleTiub@users.noreply.github.com>
2024-07-30 12:11:34 -0700

    Update PolygonOrRosette.py

--- PolygonOrRosette.py
+++ PolygonOrRosette.py
@@ -1,49 +1,41 @@
 import turtle
+
 t = turtle.Pen()
 number = int(turtle.numinput("Number of sides or circles",
-             "How many sides or circles in your shape?", 6))
+                             "How many sides or circles in your shape?", 6))
 shape = turtle.textinput("Which shape do you want?",
                          "Enter 'p' for polygon or 'r' for rosette:")
 t.speed(0)
 turtle.Screen().setup(width=0.5, height=0.75, startx=None, starty=None)
-for x in range(number):
-    if shape == 'r':
-        t.circle(100)
-        t.circle(90)
-    else:
-        t.forward (150)
-    t.left(360/number)
-    t.forward(20)
+
 t.penup()
 t.right(90)
 t.forward(500)
 t.left(90)
 t.pendown()
-t.pencolor("sky blue")
+t.pencolor("red")
 for x in range(number):
     if shape == 'r':
         t.circle(100)
         t.circle(90)
     else:
-        t.forward (150)
-    t.left(360/number)
+        t.forward(150)
+    t.left(360 / number)
     t.forward(20)
 
-
-    
 t.penup()
 t.forward(500)
 t.left(90)
 t.forward(400)
 t.pendown()
-t.pencolor("orange")
+t.pencolor("blue")
 for x in range(number):
     if shape == 'r':
         t.circle(100)
         t.circle(90)
     else:
-        t.forward (150)
-    t.left(360/number)
+        t.forward(150)
+    t.left(360 / number)
     t.forward(20)
 
 t.penup()
@@ -56,6 +48,6 @@ for x in range(number):
         t.circle(100)
         t.circle(90)
     else:
-        t.forward (150)
-    t.left(360/number)
+        t.forward(150)
+    t.left(360 / number)
     t.forward(20)
